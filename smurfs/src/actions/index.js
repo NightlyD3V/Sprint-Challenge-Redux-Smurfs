f894c44520364d87957c06767768c675ad754302
NightlyD3V <TylerSpaulding95@gmail.com>
2019-04-12 16:22:08 +0100

    Setup boiler-plate working on actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,14 @@
+import axios from 'axios';
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+
+export const GET_DATA = 'FETCHING_DATA';
+export const CREATE_NEW_SMURF = 'CREATE_NEW_SMURF';
+export const DELETE_SMURF = 'DELETE_SMURF';
+export const UPDATE_SMURF = 'UPDATE_SMURF';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,43 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+const getTheSmurfs = () => {
+  axios.get('http://localhost:3333/')
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+
+    })
+}
+
+const createSmurf = () => {
+  axios.post('http://localhost:3333/')
+    .then((res) => {
+      console.log(res);
+    })
+   .catch((err) => {
+      console.log(err);
+    })
+}
+
+const updateSmurf = () => {
+  axios.put('http://localhost:3333/')
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+const deleteSmurf = () => {
+  axios.deletep('http://localhost:3333/')
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
